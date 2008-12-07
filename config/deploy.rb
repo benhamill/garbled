f7e474dd80a2a4908f0aa364606093461690e804
Ben Hamill <git-commits@benhamill.com>
2008-12-07 16:54:10 -0600

    Edited deploy script for capistrano. Incomplete.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,22 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, 'enki'
+set :repository,  'git://github.com/BenHamill/enki.git'
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "~/apps/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
+default_run_options[:pty] = true
+set :branch, 'garbled'
+set :deploy_via, :remote_cache
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+#role :app, "benhamill.com"
+#role :web, "benhamill.com"
+#role :db,  "benhamill.com", :primary => true
+server 'benhamill.com', :app, :web, :db, :primary => true
+
+set :user, 'hamillbd'
+set :use_sudo, false
