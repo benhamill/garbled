7dcec89b750c04e55f8de4a9f1f5d2dd62312233
Ben Hamill <git-commits@benhamill.com>
2008-12-11 23:56:29 -0600

    Aiming at getting Passenger working.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,7 @@
 set :application, 'garbled'
 set :repository,  'git://github.com/BenHamill/garbled.git'
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
 set :deploy_to, "~/apps/#{application}"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
 set :scm, :git
 default_run_options[:pty] = true
 set :deploy_via, :remote_cache
@@ -20,3 +14,10 @@ server 'benhamill.dreamhosters.com', :app, :web, :db, :primary => true
 
 set :user, 'hamillbd'
 set :use_sudo, false
+
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch#{current_path}/tmp/restart.txt"
+  end
+end
