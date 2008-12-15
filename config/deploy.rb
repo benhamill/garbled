b03d619ea6d3586172d1975a94a9c96932692e95
Ben Hamill <git-commits@benhamill.com>
2008-12-14 21:02:43 -0600

    Adding .gitignore for database.yml and adding capistrano tasks to handle it.

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,4 +20,29 @@ namespace :deploy do
   task :restart, :roles => :app do
     run "touch #{current_path}/tmp/restart.txt"
   end
+  
+  desc 'Creates the production database.yml up.'
+  task :setup_production_database_config do
+    mysql_password = Capistrano::CLI.password_prompt 'Production MySQL Password:'
+    
+    require 'yaml'
+    spec = {
+      'production' => {
+        'adapter' => 'mysql',
+        'database' => user,
+        'username' => user,
+        'password' => mysql_password
+      }
+    }
+    
+    run "mkdir -p #{shared_path}/config"
+    put spec.to_yaml, "#{shared_path}/config/database.yml"
+  end
+  after 'deploy:setup', :setup_production_database_config
+  
+  desc 'Carries database.yml forward on update.'
+  task :copy_production_database_config do
+    run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after 'deplot:update_code', :copy_production_database_config
 end
