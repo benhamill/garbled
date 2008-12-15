fce06947f7eda07365651633986c8269406198a8
Ben Hamill <git-commits@benhamill.com>
2008-12-14 22:20:10 -0600

    Moved database.yml studd out of deploy namespace.

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,32 +20,32 @@ namespace :deploy do
   task :restart, :roles => :app do
     run "touch #{current_path}/tmp/restart.txt"
   end
-  
-  desc 'Creates the production database.yml up.'
-  task :setup_production_database_config do
-    mysql_password = Capistrano::CLI.password_prompt 'Production MySQL Password:'
-    
-    require 'yaml'
-    spec = {
-      'production' => {
-        'adapter' => 'mysql',
-        'database' => 'benhamill_garbled_production',
-        'username' => 'garbled',
-        'password' => mysql_password,
-        'encoding' => 'utf8',
-        'pool' => 5,
-        'host' => 'mysql.benhamill.dreamhosters.com'
-      }
-    }
-    
-    run "mkdir -p #{shared_path}/config"
-    put spec.to_yaml, "#{shared_path}/config/database.yml"
-  end
-  after 'deploy:setup', :setup_production_database_config
-  
-  desc 'Carries database.yml forward on update.'
-  task :copy_production_database_config do
-    run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  end
-  after 'deploy:update_code', :copy_production_database_config
 end
+
+desc 'Creates the production database.yml up.'
+task :setup_production_database_config do
+  mysql_password = Capistrano::CLI.password_prompt 'Production MySQL Password:'
+  
+  require 'yaml'
+  spec = {
+    'production' => {
+      'adapter' => 'mysql',
+      'database' => 'benhamill_garbled_production',
+      'username' => 'garbled',
+      'password' => mysql_password,
+      'encoding' => 'utf8',
+      'pool' => 5,
+      'host' => 'mysql.benhamill.dreamhosters.com'
+    }
+  }
+  
+  run "mkdir -p #{shared_path}/config"
+  put spec.to_yaml, "#{shared_path}/config/database.yml"
+end
+after 'deploy:setup', :setup_production_database_config
+
+desc 'Carries database.yml forward on update.'
+task :copy_production_database_config do
+  run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+end
+after 'deploy:update_code', :copy_production_database_config
